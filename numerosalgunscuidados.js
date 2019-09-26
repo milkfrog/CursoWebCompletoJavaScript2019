f41c98a56426daca8adc61e791034b65f03a18cb
milkfrog <contin.marcelo@gmail.com>
2019-09-26 13:02:38 -0300

    alguns arquivos do curso

---
+++ numerosalgunscuidados.js
@@ -0,0 +1,12 @@
+console.log(7/ 0)
+// retorna Infinity
+
+console.log('10' / 2)
+// funciona a divisao 
+
+console.log('Show' * 2)
+// NaN
+
+console.log(0.1 + 0.7)
+
+console.log((10.345).toFixed(2))
