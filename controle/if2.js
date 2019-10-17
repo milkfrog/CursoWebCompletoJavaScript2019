85267e9ef3a284969176fd441e9146c3644d5e8b
milkfrog <contin.marcelo@gmail.com>
2019-10-17 16:28:02 -0300

    aulas 53 e 54

---
+++ controle/if2.js
@@ -0,0 +1,17 @@
+function teste1(num){
+    if(num > 7)
+        console.log(num)
+
+    console.log('Final')
+}
+
+teste1(6)
+teste1(8)
+
+function teste2(num) {
+    if(num > 7); { // cuidado com o ; no if ou estruturas de controle
+        console.log(num)
+    }
+}
+teste2(5)
+teste2(10)
