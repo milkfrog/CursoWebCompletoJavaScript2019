b9a14985d9a53190433e7ad896bb06085a5cfcf7
milkfrog <contin.marcelo@gmail.com>
2019-11-22 18:40:40 -0200

    até aula 117 feito.

--- array/imperativovsDeclarativo.js
+++ array/imperativovsDeclarativo.js
@@ -10,7 +10,7 @@ for (let i = 0; i < alunos.length;i++){
 }
 console.log(total1 / alunos.length)
 
-// Declarativa (mais interessante)
+// Declarativa (mais interessante) mais reuso! 
 const getNota = aluno => aluno.nota
 const soma = (total, atual) => total + atual
 const total2 = alunos.map(getNota).reduce(soma)
