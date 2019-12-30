3c4895b4ff5b8c756a3804dfa26e582510cfa429
milkfrog <contin.marcelo@gmail.com>
2019-12-30 20:22:47 -0200

    Add aula 251. Finalizado capitulo 13. Finalizado Projeto Flappy Bird

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -125,6 +125,28 @@ function Progresso() {
 //     passaro.animar()
 // },20)
 
+function estaoSobrepostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top
+    return horizontal && vertical
+}
+
+function colidiu(passaro, barreiras) {
+    let colidiu = false
+    barreiras.pares.forEach(ParDeBarreiras => {
+        if (!colidiu) {
+            const superior = ParDeBarreiras.superior.elemento
+            const inferior = ParDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(passaro.elemento, superior) || estaoSobrepostos(passaro.elemento, inferior)
+
+        }
+    })
+    return colidiu
+}
+
 function FlappyBird() {
     let pontos = 0
 
@@ -146,6 +168,10 @@ function FlappyBird() {
         const temporizador = setInterval(() => {
             barreiras.animar()
             passaro.animar()
+
+            if (colidiu(passaro, barreiras)) {
+                clearInterval(temporizador)
+            }
         },20)
     }
 }
