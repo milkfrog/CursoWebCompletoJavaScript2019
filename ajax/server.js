8576307e3de9c0cdba043f0ccc95c359e04de8b2
milkfrog <contin.marcelo@gmail.com>
2020-01-02 11:58:42 -0200

    Add aula 254

---
+++ ajax/server.js
@@ -0,0 +1,11 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/teste', (req, resp) => resp.send('ok'))
+
+app.listen(8080, () => console.log('Executando...'))
