fea81a68d3d126e0541e034b909300799f079ffc
milkfrog <contin.marcelo@gmail.com>
2020-01-06 15:09:58 -0300

    Add aula 267

---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp')
+
+gulp.task('default', () => {
+    gulp.start('copiar', 'fim')
+    
+})
+
+
+gulp.task('copiar', ['antes1', 'antes2'], () => {
+    /* 
+    gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+        //.pipe(transformacao1())
+        //.pipe(transformacao2())
+        .pipe(gulp.dest('pastaB'))
+    */
+   gulp.src('pastaA/*.txt')
+        .pipe(gulp.dest('pastaC'))
+})
+
+gulp.task('antes1', () => {
+    console.log('Antes 1!!!!')
+})
+
+gulp.task('antes2', () => {
+    console.log('Antes 2!!!!')
+})
+
+gulp.task('fim', () => {
+    console.log('Fim !!!')
+})
