e82282ba7914c57e1f136de450598b522e358eaa
milkfrog <contin.marcelo@gmail.com>
2020-01-07 14:25:34 -0300

    Add aula 276

--- gulp/spa/gulpTasks/app.js
+++ gulp/spa/gulpTasks/app.js
@@ -1,19 +1,37 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
+sass.compiler = require('node-sass')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
-function appHtml(done) {
-    done()
+function appHtml() {
+    return gulp.src('src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true}))
+        .pipe(gulp.dest('build'))
 }
 
-function appCss(done) {
-    done()
+function appCss() {
+    return gulp.src('src/assets/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({ "uglyComments": true}))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
 
-function appJs(done) {
-    done()
+function appJs() {
+    return gulp.src('src/assets/js/**/*.js')
+        .pipe(babel({ presets: ['env']})) // transforma para versoes antigas de JS para ter mais compatibilidade
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('build/assets/js'))
 }
 
-function appImgs(done) {
-    done()
+function appImgs() {
+    return gulp.src('src/assets/imgs/**/*.*')
+        .pipe(gulp.dest('build/assets/imgs'))
 }
 
 exports.tasks = gulp.parallel(appHtml, appCss, appJs, appImgs)
