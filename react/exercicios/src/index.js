0a7d332083a05231e7e4644a3739f55845a4c9aa
milkfrog <contin.marcelo@gmail.com>
2020-02-19 19:27:06 -0300

    Add aula 343

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,13 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 //import Primeiro from './componentes/Primeiro'
-import BomDia from './componentes/BomDia'
+// import BomDia from './componentes/BomDia'
 
+import  Multi from './componentes/Multiplos'
 
 
 // const elemento = <h2>React 2</h2>
 // ReactDOM.render(elemento, document.getElementById('root'))
 // $('<h1>').html('React 2') <- JQuery
 // tem q ter a primeira letra maiuscula o componente, visando diferenciar de tags padrão!!
-ReactDOM.render(<BomDia nome="Guilherme" idade={10}/>, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome='Ana'/>
+        <Multi.BoaNoite nome='Bia'/>
+    </div>
+    , document.getElementById('root'))
 
