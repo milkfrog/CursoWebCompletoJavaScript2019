cafb70c751803a74b4f504feac94fd84484adbae
milkfrog <contin.marcelo@gmail.com>
2020-02-19 10:48:23 -0300

    Add aula 340

---
+++ react/exercicios/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const elemento = <h2>React 2</h2>
+
+ReactDOM.render(elemento, document.getElementById('root'))
+
+// $('<h1>').html('React 2') <- JQuery
+
