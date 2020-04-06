8add0d922336d37843ac19e0f231952408df002e
milkfrog <contin.marcelo@gmail.com>
2020-04-06 16:15:32 -0300

    Add aula 417

---
+++ banco-relacional/criarTabelaCidades.sql
@@ -0,0 +1,16 @@
+create table if not exists cidades (
+    id INT unsigned not null auto_increment,
+    nome varchar(255) not null,
+    estado_id int unsigned not null,
+    area decimal(10,2),
+    primary key (id),
+    foreign key (estado_id) references estados (id)
+);
+
+create table if not exists teste (
+    id INT unsigned not null auto_increment primary key
+);
+
+select * from teste;
+
+drop table if exists teste;
