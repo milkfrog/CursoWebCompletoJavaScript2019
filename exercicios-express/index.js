dd4cd6eb00cf64b4412f2e6f5bd683e949db2cc6
milkfrog <contin.marcelo@gmail.com>
2020-04-08 20:30:09 -0300

    Add aula 450

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -3,8 +3,33 @@ const app = express()
 
 // use é pra qualquer tipo de requisição http
 // ou até o app.all
-app.use('/opa', (req, res) => {
-    res.send('Estou <b>bem!</b>')
+app.get('/opa', (req, res) => {
+
+    res.json({
+        data: [
+            {id: 7, name: 'Ana', position: 1},
+            {id: 34, name: 'Bia', position: 2},
+            {id: 73, name: 'Carlos', position: 3}
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
+
+    // res.json([
+        // {id: 7, name: 'Ana', position: 1},
+        // {id: 34, name: 'Bia', position: 2},
+        // {id: 73, name: 'Carlos', position: 3}
+    // ])
+
+    // res.json({
+    //     name: 'iPad 32Gb',
+    //     price: 1899.00,
+    //     discount: 0.12
+    // })
+
+    // res.send('<h1>Estou bem!</h1><br><br><br><h2>Tipo é HTML!</h2>')
 })
 
 
