66a39a6c5ac1b370780622836a19f50cafb106c9
milkfrog <contin.marcelo@gmail.com>
2020-04-08 18:53:24 -0300

    Add aula 449

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
-
-
 const app = express()
 
-app.listen(3000, () => {
+// use é pra qualquer tipo de requisição http
+// ou até o app.all
+app.use('/opa', (req, res) => {
+    res.send('Estou <b>bem!</b>')
+})
+
+
+app.listen(5000, () => {
     console.log('Backend executando')
 })
