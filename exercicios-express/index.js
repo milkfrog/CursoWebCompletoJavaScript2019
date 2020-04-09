e8a814c5d393ba04f1d88386ffc2f2a7a76540d7
milkfrog <contin.marcelo@gmail.com>
2020-04-09 12:05:24 -0300

    Add aula 453

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -11,6 +11,29 @@ app.use((req, res, next) => {
     next()
 })
 
+app.get('/clientes/relatorio', (req, res) => {
+    res.send(`Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`)
+})
+
+app.post('/corpo', (req, res) => {
+    let corpo = ''
+    req.on('data', (parte) => {
+        corpo += parte
+    })
+
+    req.on('end', () => {
+        res.send(corpo)
+    })
+
+    // req.on('end', () => {
+    //     res.json(JSON.parse(corpo))
+    // })
+})
+
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`)
+})
+
 // use é pra qualquer tipo de requisição http
 // ou até o app.all
 app.get('/opa', (req, res, next) => {
